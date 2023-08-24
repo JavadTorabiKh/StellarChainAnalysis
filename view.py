da1ef8511d1665e95950f402183fcafc34f68120
javad <j.2528840@gmail.com>
2023-08-24 15:55:01 +0330

    create front

--- view.py
+++ view.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__ ,template_folder='templates')
 app.debug = True
 
 @app.route('/')
 def index():
-    names = ['John', 'Jane', 'Alice', 'Bob']
-    return render_template('a.html', names=names)
+    return render_template('form.html')
+
+@app.route('/process', methods=['POST'])
+def process_form():
+    names = [request.form['pub1'], request.form['pub2']]
+    return redirect(url_for('result', names=names))
+
+@app.route('/result')
+def result():
+    names = request.args.getlist('names')
+    return render_template('view.html', names=names)
 
 if __name__ == '__main__':
     app.run()
