814277a8c5cb376dc738ff6fc18b66752feff41d
javad <j.2528840@gmail.com>
2025-04-12 10:12:36 +0330

    fix

--- conectToNeo4j.py
+++ conectToNeo4j.py
@@ -6,29 +6,29 @@ URI = "bolt://localhost:7687"
 AUTH = ("neo4j", "123456789")
 
 
-
-def getRecords(p1 , p2):
-    with GraphDatabase.driver(URI, auth=AUTH) as driver: 
+def getRecords(p1, p2):
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
         driver.verify_connectivity()
     records, summary, keys = driver.execute_query(
-        'MATCH ('+p1+':NODE {name:"'+p1+'"}),('+p2+':NODE {name:"'+p2+'"}), p = shortestPath(('+p1+')-[*]->('+p2+')) RETURN p  as name',
+        'MATCH ('+p1+':NODE {name:"'+p1+'"}),('+p2+':NODE {name:"'+p2 +
+        '"}), p = shortestPath(('+p1+')-[*]->('+p2+')) RETURN p  as name',
         database_="neo4j",
     )
     driver.close()
     return records
 
- 
-def getData(p1,p2):
+
+def getData(p1, p2):
     account = []
-    amount = [] 
-    records = getRecords(p1,p2)
-    for record in records:  
+    amount = []
+    records = getRecords(p1, p2)
+    for record in records:
         for i in record["name"]:
-            a1=str(i)[188:244]
-            a2=str(i)[359:415]
-            a3=str(i)[457:471]
+            a1 = str(i)[188:244]
+            a2 = str(i)[359:415]
+            a3 = str(i)[457:471]
             amount.append(a3)
-            
+
             if a1 not in account:
                 account.append(a1)
             if a2 not in account:
@@ -36,6 +36,7 @@ def getData(p1,p2):
     return account, amount
 
 
-# ("GD3ZN2T6QQMKLAGKKMWHHU4FITANVJBTREEJERHTWGWF2GSBPOSHGCLU","GBT67MMDODT5NTYWQGDUFEQGMF2MQT5JHOUFYHVXOIB4PL3QSOPRNAHO")        
+# usage :
+# ("GD3ZN2T6QQMKLAGKKMWHHU4FITANVJBTREEJERHTWGWF2GSBPOSHGCLU","GBT67MMDODT5NTYWQGDUFEQGMF2MQT5JHOUFYHVXOIB4PL3QSOPRNAHO")
 # print(account)
 # print(amount)
