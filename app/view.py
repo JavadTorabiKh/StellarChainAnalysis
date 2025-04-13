4193e3c409f650b0e384f4474e9d7fbc43050d72
javad <j.2528840@gmail.com>
2025-04-13 10:14:06 +0330

    config

--- app/view.py
+++ app/view.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-import conectToNeo4j as con
+import app.conectToNeo4j as con
 
-app = Flask(__name__ ,template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 # app.debug = True
 
 
@@ -9,16 +9,19 @@ app = Flask(__name__ ,template_folder='templates')
 def index():
     return render_template('form.html')
 
+
 @app.route('/process', methods=['POST'])
 def process_form():
-    account, amount = con.getData(request.form['pub1'],request.form['pub2'])
+    account, amount = con.getData(request.form['pub1'], request.form['pub2'])
     print(account)
     return redirect(url_for('result', names=account))
+
 
 @app.route('/result')
 def result():
     names = request.args.getlist('names')
     return render_template('view.html', names=names)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
